3866b241755db6658c81908e5c97c67440b71eab
guillermo <guillermo.arana1@gmial.com>
2023-06-19 10:42:59 +0200

    Validacion inicial

---
+++ EXAMEN-ENDE_EV2-PRACTICO_FICHEROS/src/optimizacionCodigo/Doctor.java
@@ -0,0 +1,95 @@
+package  optimizacionCodigo ;
+
+import java.util.Date;
+
+public class Doctor extends Persona {
+	public void realizarDiagnostico() {
+	}
+
+	public void vertificarAlta() {
+	}
+
+	@Override
+	public void visitarPaciente() {
+		// TODO Auto-generated method stub
+		super.visitarPaciente();
+	}
+
+	@Override
+	public String getNombre() {
+		// TODO Auto-generated method stub
+		return super.getNombre();
+	}
+
+	@Override
+	public void setNombre(String nombre) {
+		// TODO Auto-generated method stub
+		super.setNombre(nombre);
+	}
+
+	@Override
+	public String getApellido() {
+		// TODO Auto-generated method stub
+		return super.getApellido();
+	}
+
+	@Override
+	public void setApellido(String apellido) {
+		// TODO Auto-generated method stub
+		super.setApellido(apellido);
+	}
+
+	@Override
+	public Date getFechaDeNacimiento() {
+		// TODO Auto-generated method stub
+		return super.getFechaDeNacimiento();
+	}
+
+	@Override
+	public void setFechaDeNacimiento(Date fechaDeNacimiento) {
+		// TODO Auto-generated method stub
+		super.setFechaDeNacimiento(fechaDeNacimiento);
+	}
+
+	@Override
+	public int getNumeroColegiadoMedicina() {
+		// TODO Auto-generated method stub
+		return super.getNumeroColegiadoMedicina();
+	}
+
+	@Override
+	public void setNumeroColegiadoMedicina(int numeroColegiadoMedicina) {
+		// TODO Auto-generated method stub
+		super.setNumeroColegiadoMedicina(numeroColegiadoMedicina);
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+	}
+}
---
+++ EXAMEN-ENDE_EV2-PRACTICO_FICHEROS/src/optimizacionCodigo/Enfermero.java
@@ -0,0 +1,65 @@
+package optimizacionCodigo;
+
+import java.util.Date;
+
+public class Enfermero extends Persona {
+	public void realizarCuras() {
+	}
+
+	public void contactarFamilias() {
+	}
+
+	@Override
+	public void visitarPaciente() {
+		// TODO Auto-generated method stub
+		super.visitarPaciente();
+	}
+
+	@Override
+	public String getNombre() {
+		// TODO Auto-generated method stub
+		return super.getNombre();
+	}
+
+	@Override
+	public void setNombre(String nombre) {
+		// TODO Auto-generated method stub
+		super.setNombre(nombre);
+	}
+
+	@Override
+	public String getApellido() {
+		// TODO Auto-generated method stub
+		return super.getApellido();
+	}
+
+	@Override
+	public void setApellido(String apellido) {
+		// TODO Auto-generated method stub
+		super.setApellido(apellido);
+	}
+
+	@Override
+	public Date getFechaDeNacimiento() {
+		// TODO Auto-generated method stub
+		return super.getFechaDeNacimiento();
+	}
+
+	@Override
+	public void setFechaDeNacimiento(Date fechaDeNacimiento) {
+		// TODO Auto-generated method stub
+		super.setFechaDeNacimiento(fechaDeNacimiento);
+	}
+
+	@Override
+	public int getNumeroColegiadoMedicina() {
+		// TODO Auto-generated method stub
+		return super.getNumeroColegiadoMedicina();
+	}
+
+	@Override
+	public void setNumeroColegiadoMedicina(int numeroColegiadoMedicina) {
+		// TODO Auto-generated method stub
+		super.setNumeroColegiadoMedicina(numeroColegiadoMedicina);
+	}
+}
